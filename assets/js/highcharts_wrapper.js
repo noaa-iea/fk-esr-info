8b32a2eceb02b5c225fae21eee81b9604ecc930c
superjai <jai.ranganathan@gmail.com>
2021-03-25 16:54:55 -0400

    update

--- assets/js/highcharts_wrapper.js
+++ assets/js/highcharts_wrapper.js
@@ -11,6 +11,10 @@ function createGraph(theData, graph_location, title, y_axis_label, x_axis_label 
     var linenum = theCsvLines.length - 1;
     var lastRow = theCsvLines[linenum];
     var lastDate = lastRow.split(",")[0];
+    
+    // Find first year in data, with a csv generally outputted by google sheets
+    var firstDate = theCsvLines[0].split(",")[0];
+    
     //If the csv isn't outputted by google sheets, this extra step needed
     if (lastDate.length === 0) {
       lastRow = theCsvLines[linenum-1];
@@ -18,15 +22,24 @@ function createGraph(theData, graph_location, title, y_axis_label, x_axis_label 
     }
 
     //The dates in the SST time series data are formatted differently than all of the other time series, so the x-axis plot bands need to be calculated differently for SST data 
+    
+    var plotBandLabel = "Trends Since 2011";
     if (SST === false){
-      begin_plot_band = lastDate - plot_band_width; 
+      if (firstDate > 2011){
+        begin_plot_band = firstDate;
+        plotBandLabel = "Trends Since " + firstDate;    
+      }
+      else {
+        begin_plot_band = 2011; //lastDate - plot_band_width;
+      }
       end_plot_band = lastDate;
     }
     else{
       date_breakout = lastDate.split("/");
-      begin_plot_band = Date.UTC(date_breakout[2] - plot_band_width, date_breakout[0], date_breakout[1]); 
+      begin_plot_band = Date.UTC(2011,1,1);  //Date.UTC(date_breakout[2] - plot_band_width, date_breakout[0], date_breakout[1]); 
       end_plot_band = Date.UTC(date_breakout[2], date_breakout[0], date_breakout[1]);
     }
+    
     // Produce highcharts graph
     Highcharts.chart(graph_location, {
       title: {text: title},
@@ -43,7 +56,9 @@ function createGraph(theData, graph_location, title, y_axis_label, x_axis_label 
         plotBands: [{
           from: begin_plot_band, 
           to: end_plot_band,
-          color: '#DEDEFF'}],
+          color: '#DEDEFF',
+          label: {text: plotBandLabel, align: 'center'}
+        }],
         title: {text: x_axis_label}},
       yAxis: {
         title: {text: y_axis_label} }});
