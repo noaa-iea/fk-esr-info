4965b9ea919db005a07c56d29552138cbdeb61a4
superjai <jai.ranganathan@gmail.com>
2021-04-09 11:01:13 -0400

    update

--- assets/js/infographiq-new.js
+++ assets/js/infographiq-new.js
@@ -21,7 +21,7 @@ function basename(path) {
 }
 
 // main function to link svg elements to modal popups with data in csv
-function link_svg(svg, csv, svg_location, csv_location, debug = false, hover_color = 'yellow', width = '100%', height = '100%', modal_id = 'modal', modal_url_pfx = 'https://marinebon.org/fk-esr-info/', colored_sections = false, section_colors = ['green', 'purple', 'orange']) {
+function link_svg(svg, csv, svg_location, csv_location, debug = false, hover_color = 'yellow', width = '100%', height = '100%', modal_id = 'modal', modal_url_pfx = 'https://marinebon.org/fk-esr-info/', text_list = true, colored_sections = false, section_colors = ['green', 'purple', 'orange']) {
   
   d3.xml(svg).then((f) => {
     
@@ -39,173 +39,179 @@ function link_svg(svg, csv, svg_location, csv_location, debug = false, hover_col
     if (debug){ 
       console.log('before data.forEach');
     }
-    
-  d3.csv(csv).then(function(data) {
+  
 
-    // TODO: if has section column in argument to fxn
-    data = data.sort(
-      function(a,b) { return d3.ascending(a.section, b.section) ||  d3.ascending(a.title, b.title) });
-
-    var section_list = [];
-
-    data.forEach(function(d) {
-      if (section_list.length == 0 | section_list[section_list.length - 1] != d.section){
-        section_list.push(d.section);
-      }
-    })
-
-    section_color_index = 0;
-    
-    d3.select("#" + csv_location)
-        .attr("class", "panel-group")
-        .attr("role", "tablist")
-        .attr("aria-multiselectable", "true");
-    
-    for (let i = 0; i < section_list.length; i++) {
-      d3.select("#" + csv_location).append("div")
-        .attr("id", csv_location + "Panel" + i)
-        .attr("class", "panel panel-default");
-      d3.select("#" + csv_location + "Panel" + i).append("div")
-        .attr("id", csv_location + "PanelSub" + i)
-        .attr("class", "panel-heading")
-        .attr("role", "tab");
-      if (colored_sections === true){
-        d3.select("#" + csv_location + "PanelSub" + i).attr("style", "background-color: " + section_colors[section_color_index] +  ";");
-        section_color_index++;
-        if (section_colors.length == section_color_index){section_color_index = 0;}
-        
-      }
-      d3.select("#" + csv_location + "PanelSub" + i).append("h4")
-        .text(section_list[i])
-        .attr("id", csv_location + "PanelTitle" + i)
-        .attr("class", "panel-title")
-        .attr("data-toggle", "collapse")
-        .attr("data-target", "#" + csv_location + "collapse" + i)
-        .attr("role", "button")
-        .attr("aria-controls", csv_location + "collapse" + i);
-      d3.select("#" + csv_location + "Panel" + i).append("div")
-          .attr("id", csv_location + "collapse" + i) 
-          .attr("role", "tabpanel")
-          .attr("aria-labelledby", "heading" + i);
-      if (i == 0) {
-        d3.select("#" + csv_location + "PanelTitle" + i).attr("aria-expanded", "true");
-        d3.select("#" + csv_location + "collapse" + i) .attr("class", "panel-collapse collapse in");
-      }
-      else {
-        d3.select("#" + csv_location + "PanelTitle" + i).attr("aria-expanded", "false");
-        d3.select("#" + csv_location + "collapse" + i) .attr("class", "panel-collapse collapse");
-      }
-
-      d3.select("#" + csv_location + "collapse" + i).append("div")
-        .attr("class", "panel-body")
-        .attr("id", csv_location + "body" + i);
-
-      var section_content = d3.select('#' + csv_location + 'body' + i).append('ul');
-
+  
+    d3.csv(csv).then(function(data) {
+  
+      // TODO: if has section column in argument to fxn
+      data = data.sort(
+        function(a,b) { return d3.ascending(a.section, b.section) ||  d3.ascending(a.title, b.title) });
+  
+      var section_list = [];
+  
       data.forEach(function(d) {
-        if (d.section == section_list[i]){
-          if (debug){ 
-            console.log('forEach d.icon: ' + d.icon);
-          }
-          // TODO: wrap this hack into function args
-          d.link = modal_url_pfx + 'modals/' + d.icon + '.html';
-          d.title = d.title ? d.title : d.icon;  // fall back on id if title not set
-
-          function handleClick(){
-            if (d.not_modal == 'T'){
-              window.location = d.link;
-            } else {
-              
-              if (debug){ 
-                console.log('  link:' + d.link);
-              }
-              
-              // https://www.drupal.org/node/756722#using-jquery
-              (function ($) {
-                $('#'+ modal_id).find('iframe')
-                  .prop('src', function(){ return d.link });
-                
-                $('#'+ modal_id + '-title').html( d.title );
-                
-                $('#'+ modal_id).on('show.bs.modal', function () {
-                  $('.modal-content').css('height',$( window ).height()*0.9);
-                  $('.modal-body').css('height','calc(100% - 65px - 55.33px)');
-                });
-                
-                $('#'+ modal_id).modal();
-              }(jQuery));
-            }
-          }
-
-          function handleMouseOver(){
-            if (debug){ 
-                console.log('  mouseover():' + d.icon);
-            }
-             
-            d3.select('#' + d.icon)
-              .style("stroke-width", 2)
-              .style("stroke", hover_color);
-            
-            tooltip_div.transition()
-              .duration(200)
-              .style("opacity", 0.8);
-            tooltip_div.html(d.title + "<br/>")
-              .style("left", (d3.event.pageX) + "px")
-              .style("top", (d3.event.pageY - 28) + "px");
-          }
-
-          function handleMouseOverSansTooltip(){
-            if (debug){ 
-                console.log(' handleMouseOverSansTooltip():' + d.icon);
-            }
-             
-            d3.select('#' + d.icon)
-              .style("stroke-width", 2)
-              .style("stroke", hover_color);
-          }
-
-          function handleMouseOut(){
-            if (debug){ 
-                console.log('  mouseout():' + d.icon);
-              }
-              
-              d3.select('#' + d.icon)
-                .style("stroke-width",0);
-    
-              tooltip_div.transition()
-                .duration(500);
-              tooltip_div.style("opacity", 0);
-          }
-
-          h.select('#' + d.icon)
-            .on("click", handleClick)
-            .on('mouseover', handleMouseOver)
-            .on('mouseout', handleMouseOut);
-
-            // set outline of paths within group to null
-            d3.select('#' + d.icon).selectAll("path")
-                .style("stroke-width", null)
-                .style("stroke", null);
-
-          // add to bulleted list of svg elements
-          list_text = d.title ? d.title : d.icon;  // fall back on id if title not set
-
-          section_content.append("li").append("a")
-            .text(list_text)
-            .on("click", handleClick)
-            .on('mouseover', handleMouseOverSansTooltip)
-            .on('mouseout', handleMouseOut);
+        if (section_list.length == 0 | section_list[section_list.length - 1] != d.section){
+          section_list.push(d.section);
         }
       })
-    }
-  }) // end: d3.csv().then({
-    
+  
+      section_color_index = 0;
+      
+      if (text_list === false){csv_location="nowhere"};
+      
+      d3.select("#" + csv_location)
+          .attr("class", "panel-group")
+          .attr("role", "tablist")
+          .attr("aria-multiselectable", "true");
+      
+      for (let i = 0; i < section_list.length; i++) {
+        d3.select("#" + csv_location).append("div")
+          .attr("id", csv_location + "Panel" + i)
+          .attr("class", "panel panel-default");
+        d3.select("#" + csv_location + "Panel" + i).append("div")
+          .attr("id", csv_location + "PanelSub" + i)
+          .attr("class", "panel-heading")
+          .attr("role", "tab");
+        if (colored_sections === true){
+          d3.select("#" + csv_location + "PanelSub" + i).attr("style", "background-color: " + section_colors[section_color_index] +  ";");
+          section_color_index++;
+          if (section_colors.length == section_color_index){section_color_index = 0;}
+          
+        }
+        d3.select("#" + csv_location + "PanelSub" + i).append("h4")
+          .text(section_list[i])
+          .attr("id", csv_location + "PanelTitle" + i)
+          .attr("class", "panel-title")
+          .attr("data-toggle", "collapse")
+          .attr("data-target", "#" + csv_location + "collapse" + i)
+          .attr("role", "button")
+          .attr("aria-controls", csv_location + "collapse" + i);
+        d3.select("#" + csv_location + "Panel" + i).append("div")
+            .attr("id", csv_location + "collapse" + i) 
+            .attr("role", "tabpanel")
+            .attr("aria-labelledby", "heading" + i);
+        if (i == 0) {
+          d3.select("#" + csv_location + "PanelTitle" + i).attr("aria-expanded", "true");
+          d3.select("#" + csv_location + "collapse" + i) .attr("class", "panel-collapse collapse in");
+        }
+        else {
+          d3.select("#" + csv_location + "PanelTitle" + i).attr("aria-expanded", "false");
+          d3.select("#" + csv_location + "collapse" + i) .attr("class", "panel-collapse collapse");
+        }
+  
+        d3.select("#" + csv_location + "collapse" + i).append("div")
+          .attr("class", "panel-body")
+          .attr("id", csv_location + "body" + i);
+  
+        var section_content = d3.select('#' + csv_location + 'body' + i).append('ul');
+  
+        data.forEach(function(d) {
+          if (d.section == section_list[i]){
+            if (debug){ 
+              console.log('forEach d.icon: ' + d.icon);
+            }
+            // TODO: wrap this hack into function args
+            d.link = modal_url_pfx + 'modals/' + d.icon + '.html';
+            d.title = d.title ? d.title : d.icon;  // fall back on id if title not set
+  
+            function handleClick(){
+              if (d.not_modal == 'T'){
+                window.location = d.link;
+              } else {
+                
+                if (debug){ 
+                  console.log('  link:' + d.link);
+                }
+                
+                // https://www.drupal.org/node/756722#using-jquery
+                (function ($) {
+                  $('#'+ modal_id).find('iframe')
+                    .prop('src', function(){ return d.link });
+                  
+                  $('#'+ modal_id + '-title').html( d.title );
+                  
+                  $('#'+ modal_id).on('show.bs.modal', function () {
+                    $('.modal-content').css('height',$( window ).height()*0.9);
+                    $('.modal-body').css('height','calc(100% - 65px - 55.33px)');
+                  });
+                  
+                  $('#'+ modal_id).modal();
+                }(jQuery));
+              }
+            }
+  
+            function handleMouseOver(){
+              if (debug){ 
+                  console.log('  mouseover():' + d.icon);
+              }
+               
+              d3.select('#' + d.icon)
+                .style("stroke-width", 2)
+                .style("stroke", hover_color);
+              
+              tooltip_div.transition()
+                .duration(200)
+                .style("opacity", 0.8);
+              tooltip_div.html(d.title + "<br/>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            }
+  
+            function handleMouseOverSansTooltip(){
+              if (debug){ 
+                  console.log(' handleMouseOverSansTooltip():' + d.icon);
+              }
+               
+              d3.select('#' + d.icon)
+                .style("stroke-width", 2)
+                .style("stroke", hover_color);
+            }
+  
+            function handleMouseOut(){
+              if (debug){ 
+                  console.log('  mouseout():' + d.icon);
+                }
+                
+                d3.select('#' + d.icon)
+                  .style("stroke-width",0);
+      
+                tooltip_div.transition()
+                  .duration(500);
+                tooltip_div.style("opacity", 0);
+            }
+  
+            h.select('#' + d.icon)
+              .on("click", handleClick)
+              .on('mouseover', handleMouseOver)
+              .on('mouseout', handleMouseOut);
+  
+              // set outline of paths within group to null
+              d3.select('#' + d.icon).selectAll("path")
+                  .style("stroke-width", null)
+                  .style("stroke", null);
+  
+            // add to bulleted list of svg elements
+            list_text = d.title ? d.title : d.icon;  // fall back on id if title not set
+  
+            section_content.append("li").append("a")
+              .text(list_text)
+              .on("click", handleClick)
+              .on('mouseover', handleMouseOverSansTooltip)
+              .on('mouseout', handleMouseOut);
+          }
+        })
+      }
+    }) // end: d3.csv().then({
+  
     .catch(function(error){
       // d3.csv() error   
     }); // end: d3.csv()
     
   // turn off questions by default
   d3.select("#text").attr("display", "none");
+
+
 
   }); // d3.xml(svg).then((f) => {
 
